4d0ef0eb31c01c1379020f9a971e1f54c755f21a
Daria Neshcheretnaya <dashkan1994@gmail.com>
2023-10-05 11:54:19 +0100

    Remove folder

--- FixedFirstHW_JS/Task-1_fixed.js
+++
@@ -1,9 +0,0 @@
-//Задача 1
-
-const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-    
-const initials = userNames.map(name => {
-    return name.split(" ").map(item => item[0].toUpperCase()).join('.');
-}).sort(); 
-
-console.log(initials); // [ 'Г.П.А', 'П.О.І', 'Р.А.О' ]
--- FixedFirstHW_JS/Task-3_fixed.js
+++
@@ -1,7 +0,0 @@
-//Задача 3
-
-const currentMaxValue = 4589;
-let reverseMaxValue = Number(currentMaxValue.toString().split("").reverse().join(""));
-
-console.log(reverseMaxValue); // 9854
-console.log(typeof reverseMaxValue); // 'number'
