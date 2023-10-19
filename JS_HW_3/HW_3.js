77525ce3c656766f418d5c1dc1d47f0a8fec97bd
Daria Neshcheretnaya <dashkan1994@gmail.com>
2023-10-19 17:00:54 +0300

    WTF

--- JS_HW_3/HW_3.js
+++ JS_HW_3/HW_3.js
@@ -28,7 +28,55 @@ function multiply(a) {
 
 //Задача 3
 
+const movies = [
+	{
+		movieName: 'The Thing',
+		releaseYear: 1982,
+		directedBy: 'Carpenter',
+		runningTimeInMinutes: 109,
+	},
+	{
+		movieName: 'Aliens',
+		releaseYear: 1986,
+		directedBy: 'Cameron',
+		runningTimeInMinutes: 137,
+	},
+	{
+		movieName: 'Men in Black',
+		releaseYear: 1997,
+		directedBy: 'Sonnenfeld',
+		runningTimeInMinutes: 98,
+	},
+	{
+		movieName: 'Predator',
+		releaseYear: 1987,
+		directedBy: 'McTiernan',
+		runningTimeInMinutes: 107,
+	},
+];
 
+//console.log(movies.sort(byProperty('releaseYear', '>'))); 
+// виведе масив фільмів посортованих по року випуску, від старішого до новішого
+//console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); 
+// виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
+//console.log(movies.sort(byProperty('movieName', '>'))); 
+// виведе масив фільмів посортованих по назві, в алфавітному порядку
+
+function byProperty(property, direction) {
+return function (a, b) {
+	if (direction === '>'){
+		if (a[property] > b[property]) return 1;
+		if (a[property] < b[property]) return -1;
+	}
+	if (direction === '<'){
+		if (a[property] < b[property]) return 1;
+		if (a[property] > b[property]) return -1;
+	}
+
+	return 0;
+} 
+	
+};
 
 
 
