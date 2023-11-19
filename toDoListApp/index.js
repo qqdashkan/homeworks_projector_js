58b715c5c4c5a62032c3fe78009f02c0534db2b4
Daria Neshcheretnaya <dashkan1994@gmail.com>
2023-11-19 17:24:00 +0200

    toDoListApp

--- toDoListApp/index.js
+++ toDoListApp/index.js
@@ -5,6 +5,20 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const form = document.querySelector('.create-task-form');
 
+const addBtn = document.querySelector('.btn');
+const containerBody = document.querySelector('.container');
+
+taskList.classList.add('font');
+containerBody.style.margin = '50px';
+addBtn.style.marginTop = '20px';
+
+
+const titles = document.getElementsByTagName('h3');
+for (let title of titles) {
+    title.classList.add('font');
+
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (taskInput.value.trim() === ''){
@@ -20,11 +34,22 @@ form.addEventListener('submit', (event) => {
 
 document.addEventListener('DOMContentLoaded', renderTasks);
 
+taskList.addEventListener('mouseover', (event) => {
+    let target = event.target.closest('li');
+    if(!target) return;
+    target.classList.toggle('color');      
+});
+
+taskList.addEventListener('mouseout', (event) => {
+    let target = event.target.closest('li');
+    if(!target) return;
+    target.classList.toggle('color'); 
+});
+
 taskList.addEventListener('click', (event) => {
     const iconContainer = event.target.parentElement;
     
-
-    if(iconContainer.classList.contains('delete-item')) {
+    if (iconContainer.classList.contains('delete-item')) {
         iconContainer.parentElement.setAttribute('status', 'delete');
  
         if (confirm('Are you sure?')) {
@@ -43,15 +68,20 @@ taskList.addEventListener('click', (event) => {
         }
     }
 
-    if(iconContainer.classList.contains('edit-element')) {
+    if (iconContainer.classList.contains('edit-element')) {
+            let newTask = window.prompt('New name', iconContainer.parentElement.textContent);
 
-            const newTask = window.prompt('Edit task');
+            if (newTask === null){
+                return;
+            }
             iconContainer.parentElement.innerHTML = newTask;
-            let tasks = Array.from(document.querySelectorAll('.collection-item'));
 
+            let tasks = Array.from(document.querySelectorAll('.collection-item'));
             const newTaskList = tasks.map(task => task.textContent);
             localStorage.setItem('tasks', JSON.stringify(newTaskList));
-    }
+        }
+
+
 })
 
 
