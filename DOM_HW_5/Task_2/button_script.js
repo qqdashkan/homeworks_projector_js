526cfb30f0ad307a8e68afc65d0f430741e9d84a
Daria Neshcheretnaya <dashkan1994@gmail.com>
2023-12-10 15:13:27 +0200

    Updated Task 2 Button

--- DOM_HW_5/Task_2/button_script.js
+++ DOM_HW_5/Task_2/button_script.js
@@ -3,51 +3,47 @@
 const button = document.querySelector('.btn');
 
 const elem = document.createElement('div');
-document.body.appendChild(elem);
 elem.classList.add('style-text');
 
-
-
 function showLastTimeChange() {
-    let time = new Date().toISOString().slice(0,19).split('T').join(' ');
-    localStorage.setItem('lastTimeChange', time);
+
+    document.body.appendChild(elem);
 
     let lastChange = localStorage.getItem('lastTimeChange');
 
     if (document.body.classList.contains('dark-color-background')) {
+        document.body.classList.remove('white-color-background');
         button.innerHTML = 'Turn On';
         elem.innerHTML = `Last turn off: ${lastChange}`;
-
     } 
     else {
+        document.body.classList.add('white-color-background');
         button.innerHTML = 'Turn Off';
         elem.innerHTML = `Last turn on: ${lastChange}`;
-
     }
 
-    localStorage.setItem('currentBtn', button.innerHTML); 
-    localStorage.setItem('currentStatus', elem.innerHTML); 
     localStorage.setItem('currentColor', document.body.className); 
 }
 
-
 button.addEventListener('click', function () {
     document.body.classList.toggle('dark-color-background');
+
+    let time = new Date().toISOString().slice(0,19).split('T').join(' ');
+    localStorage.setItem('lastTimeChange', time);
 
     showLastTimeChange();
 
 });
 
     document.addEventListener("DOMContentLoaded", function () {
+    
+    localStorage.length < 2 ? elem.innerHTML.remove() : elem.innerHTML = this.textContent;
 
     let currentBackGround = localStorage.getItem('currentColor');
-    document.body.className = currentBackGround;
 
-    let currentButton = localStorage.getItem('currentBtn');
-    button.innerHTML = currentButton;
+    !currentBackGround ? document.body.classList.add('white-color-background') : document.body.className = currentBackGround;
 
-    let lastStatus = localStorage.getItem('currentStatus');
-    elem.innerHTML = lastStatus;
+    showLastTimeChange();
 
 }); 
 
